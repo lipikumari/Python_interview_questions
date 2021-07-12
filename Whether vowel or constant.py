b9b110c5275e49c05ba838ca5f1b4c40f203cbfa
Lipi Kumari <47522378+lipikumari@users.noreply.github.com>
2021-07-12 23:45:41 +0530

    Add files via upload

---
+++ Whether vowel or constant.py
@@ -0,0 +1,6 @@
+#Checking a character is a vowel or constant
+char=input()
+if(char == 'a' , char == 'e' , char=='i' , char=='o' , char=='u'):
+    print('Character is in vowel')
+else:
+    print('Character is in constant')
